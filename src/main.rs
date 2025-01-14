9661cb204a264e2e82c8535d946a61c3ec0a127b
deep60 <deep6060p@gmail.com>
2025-01-15 00:01:25 +0530

    add pkg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> Result<()> {
+    println!("{}", BANNER);
+
+    let mut input = String::new();
+    println!("");
+    io::stdin().read_line(&mut input)?;
+    let listname = input.trim().to_string();
+
+    input.clear();
+    println!("");
+    io::stdin().read_line(&mut input)?;
+    let proxylist = input.trim().to_string();
 }
